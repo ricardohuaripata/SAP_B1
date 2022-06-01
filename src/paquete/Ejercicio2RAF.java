5f07d37f95f28e1027e5af348e511aa74c23f466
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2022-06-01 12:49:17 +0200

    m

---
+++ src/paquete/Ejercicio2RAF.java
@@ -0,0 +1,59 @@
+package paquete;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public class Ejercicio2RAF {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		File fichero = new File("C:\\files\\ficheroprueba.txt");
+
+		System.out.print("Escribir palabra para modificar a mayusculas: ");
+		String palabra = sc.nextLine();
+
+		if (fichero.exists())
+			modificarFichero(palabra, fichero);
+		else
+			System.out.println("El fichero no existe");
+
+	}
+
+	public static void modificarFichero(String palabra, File fichero) {
+
+		try {
+
+			RandomAccessFile raf = new RandomAccessFile(fichero, "rw");
+			String cadena;
+			cadena = raf.readLine();
+			long pos = 0;
+
+			while (cadena != null) {
+
+				if (cadena.indexOf(palabra) >= 0) {
+
+					cadena = cadena.replace(palabra, palabra.toUpperCase());
+					System.out.println(palabra.toUpperCase());
+					raf.seek(pos);
+					raf.writeBytes(cadena);
+
+				}
+
+				pos = raf.getFilePointer();
+				cadena = raf.readLine();
+
+			}
+			
+			raf.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+	}
+
+}
---
+++ src/paquete/Ejercicio3RAF.java
@@ -0,0 +1,67 @@
+package paquete;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public class Ejercicio3RAF {
+
+	static RandomAccessFile raf;
+
+	public static void main(String[] args) throws IOException { // 79-84 examen
+
+		Scanner sc = new Scanner(System.in);
+
+		File f = new File("C:\\files\\productos.dat");
+
+		raf = new RandomAccessFile(f, "rw");
+
+		int codproducto;
+		String nombre;
+		double precio;
+		long pos = f.length();
+
+		for (int i = 0; i < 12; i++) {
+			
+			System.out.print("\nCodigo del producto: ");
+			codproducto = sc.nextInt();
+			sc.nextLine();
+			
+			System.out.print("Nombre del producto: ");
+			nombre = sc.nextLine();
+			
+			StringBuffer nombrebuff = new StringBuffer(nombre);
+			nombrebuff.setLength(30);
+
+			System.out.print("Precio del producto: ");
+			precio = sc.nextDouble();
+			
+			pos += (4 + (2 * 30) + 8);
+
+			guardarProducto(pos, codproducto, nombrebuff, precio);
+
+		}
+
+		raf.close();
+
+	}
+
+	public static void guardarProducto(long pos, int codproducto, StringBuffer nombre, double precio) {
+
+		try {
+			raf.seek(pos);
+			raf.writeInt(codproducto);
+			raf.writeChars(nombre.toString());
+			raf.writeDouble(precio);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	// metodo para preguntar al usuario si quiere modificar el campo de un registro
+
+}
+
